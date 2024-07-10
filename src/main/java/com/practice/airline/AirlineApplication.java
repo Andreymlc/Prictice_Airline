cf88bcbffeddc3cc77c8c920b76581f12c8570d5
Andrey Maltsev <zeeezya@gmail.com>
2024-07-10 16:21:54 +0300

    connecting to the database

--- src/main/java/com/practice/airline/AirlineApplication.java
+++ src/main/java/com/practice/airline/AirlineApplication.java
@@ -2,8 +2,10 @@ package com.practice.airline;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 
 @SpringBootApplication
+@EntityScan(basePackages = {"Models"})
 public class AirlineApplication {
 
     public static void main(String[] args) {
